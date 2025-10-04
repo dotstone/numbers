9d6ffabbd98714e64d1be3e7dd9f6b9a6634be43
dos <d.steinbinder@gmail.com>
2025-10-04 21:21:42 +0200

    Remove organization folder from Job DSL - keep only explicit microservice jobs

--- jenkins_jobs.groovy
+++ jenkins_jobs.groovy
@@ -1,54 +1,5 @@
 // Job DSL script to create Jenkins jobs for Numbers microservices
 
-organizationFolder('numbers-services') {
-  displayName('Numbers Microservices')
-  description('Parent folder for all Numbers microservices')
-  
-  organizations {
-    github {
-      repoOwner('dotstone')
-      apiUri('https://api.github.com')
-      credentialsId('github-pat-numbers')
-      
-      traits {
-        gitHubBranchDiscovery {
-          strategyId(1)
-        }
-        gitHubPullRequestDiscovery {
-          strategyId(1)
-        }
-      }
-    }
-  }
-  
-  projectFactories {
-    workflowMultiBranchProjectFactory {
-      scriptPath('Jenkinsfile')
-    }
-  }
-  
-  configure { node ->
-    def traits = node / navigators / 'org.jenkinsci.plugins.github__branch__source.GitHubSCMNavigator' / traits
-    traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
-      includes('numbers')
-      excludes('')
-    }
-  }
-  
-  orphanedItemStrategy {
-    discardOldItems {
-      daysToKeep(-1)
-      numToKeep(10)
-    }
-  }
-  
-  triggers {
-    periodicFolderTrigger {
-      interval('1m')
-    }
-  }
-}
-
 multibranchPipelineJob('number-generator') {
   displayName('Number Generator Service')
   description('Builds and deploys the number-generator microservice')
